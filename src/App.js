c5055662656592e4ff664b0348561fb1c7548f95
MightyPhoenix <agnibesh00mukherjee@gmail.com>
2020-10-01 13:05:34 +0530

    Optimised and Added React-Router and Created a Home page with Hero TEXT

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import React from "react";
 import "./App.css";
+import { Switch, Route } from 'react-router-dom';
 
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="App">
-      <header className="nav-ani">
+      <header>
         <Navbar />
       </header>
-      <main>hi</main>
+      <main>
+        <Switch>
+          <Route exact path="/" component={Home}/>
+        </Switch>
+      </main>
       <footer><Footer/></footer>
     </div>
   );
 }
 
 export default App;
+
+const Routes = [{exact:true,path:"",component:Home}]
